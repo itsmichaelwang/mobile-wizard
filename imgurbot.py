2ab28e352a60a968228d7a4462daf984705ecfea
Michael Wang <michaelwang0712@gmail.com>
2014-06-28 21:50:22 -0500

    Tried to make image upload work, couldn't, ergo, fuck it, we'll do it tomorrow

--- imgurbot.py
+++ imgurbot.py
@@ -1,3 +1,16 @@
 import requests
+from credentials import imgur_access_token
 
 #this part of the bot uploads a specified file to imgur (will be merged in eventually)
+url = 'https://api.imgur.com/3/image'
+header = {
+	'Authorization': 'Bearer ' + imgur_access_token
+}
+payload = {
+	'image':'data.PNG',
+	'type':'file',
+	'title':"Test Image",
+	'description':"Test of the ASCII-Bot"
+}
+r = requests.post(url, data=payload, headers=header)
+print r
