b63582a4cea7437bd32dc283dd55d1a31b5a3a7b
Michael Wang <michaelwang0712@gmail.com>
2014-07-03 01:08:00 -0400

    Turned access token script into get_access_token() in tokens.py, imports from credentials file (unpushed file with access codes) to hide credentials, can now be pushed

---
+++ tokens.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from pprint import pprint
+
+from credentials import imgur_refresh_token
+from credentials import imgur_client_ID
+from credentials import imgur_client_secret
+
+def get_access_token():
+	payload = {'refresh_token': imgur_refresh_token,
+						 'client_id': imgur_client_ID,
+						 'client_secret': imgur_client_secret,
+						 'grant_type':'refresh_token'}
+	r = requests.post('https://api.imgur.com/oauth2/token', data=payload)
+	content = json.loads(r.content)
+	access_token = content['access_token']
+	
+	return access_token
