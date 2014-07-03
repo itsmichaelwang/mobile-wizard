36076439d9dd88921c6982a061d901833efe7f69
Michael Wang <michaelwang0712@gmail.com>
2014-07-03 01:20:12 -0400

    upload_image() in imgurbot.py correctly handles imgur 403 (invalid access token) errors by retrieving a new one, however the new code does not get saved to credentials file

--- imgurbot.py
+++ imgurbot.py
@@ -2,16 +2,37 @@ import requests
 from base64 import b64encode
 from credentials import imgur_access_token
 
-#this part of the bot uploads a specified file to imgur (will be merged in eventually)
-url = 'https://api.imgur.com/3/image'
-header = {
-	'Authorization': 'Bearer ' + imgur_access_token
-}
-payload = {
-	'image':b64encode(open('data.png', 'rb').read()),
-	'type':'base64',
-	'title':"Test Image",
-	'description':"Test of the ASCII-Bot"
-}
-r = requests.post(url, data=payload, headers=header)
-print r
+# required for get_access_token
+from tokens import *
+
+def upload_image(filename):
+	url = 'https://api.imgur.com/3/image'
+	#encode image for POST request
+	with open(filename, 'rb') as myfile:
+		data = b64encode(myfile.read())
+
+	payload = {
+		'image':data,
+		'type':'base64',
+		'title':"Test Image",
+		'description':"Test of the ASCII-Bot"
+	}
+	header = {
+		'Authorization': 'Bearer ' + imgur_access_token
+	}
+
+	status_code = 0
+	while (status_code != 200):
+		# invalid access token - generate a new one and print it (storing to file will come later)
+		if (status_code == 403):
+			new_access_token = get_access_token()
+			print new_access_token
+			header = {
+				'Authorization': 'Bearer ' + new_access_token
+			}
+
+		r = requests.post(url, data=payload, headers=header)
+		print r
+		status_code = r.status_code
+
+upload_image('data.png')
